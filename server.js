d8ebf84a2e24fd4e34d95b2b6b2da56ce2216500
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-01-22 21:28:59 +0200

    setup a simple get request to jsonplaceholder api

--- server.js
+++ server.js
@@ -9,7 +9,25 @@ app.use(express.urlencoded({ extended: true }))
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json())
 
+// Handle GET request to fetch users
+app.get('/users', async (req, res) => {
+	// const users = [
+	// 	{ id: 1, name: 'John Doe' },
+	// 	{ id: 2, name: 'Bob Williamas' },
+	// 	{ id: 3, name: 'Shannon Jackson' },
+	// ]
+
+	const response = await fetch('https://jsonplaceholder.typicode.com/users')
+	const users = await response.json()
+	res.send(`
+        <h1 class="text-2xl fond-bold my-4">Users</h1>
+        <ul>
+            ${users.map((user) => `<li>${user.name}</li>`).join('')}
+        </ul>
+    `)
+})
+
 // Start the server
 app.listen(3000, () => {
-	console.log(`Server listening on port 3000`)
+	console.log(`Server listening on port: http://localhost:3000`)
 })
