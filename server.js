3bcae56de5286265e6df0fa916ee923070ad609c
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-01-23 17:30:21 +0200

    added validation.html and server route to check the form input validation on the server

--- server.js
+++ server.js
@@ -94,6 +94,59 @@ app.post('/search/api', async (req, res) => {
 		res.send(searchResultsHtml)
 	}, 1000)
 })
+
+// Handle POST request for email validation
+app.post('/contact/email', (req, res) => {
+	const submittedEmail = req.body.email
+	const emailRegex = /^[A-Za-z0-9._%-]*@[A-Za-z0-9.-]*\.[A-Za-z]{2,4}$/
+
+	const isValid = {
+		message: 'That email is valid',
+		class: 'text-green-700',
+	}
+
+	const isInvalid = {
+		message: 'Please enter a valid email address',
+		class: 'text-red-700',
+	}
+
+	if (!emailRegex.test(submittedEmail)) {
+		return res.send(
+			`
+			<div class="mb-4" hx-target="this" hx-swap="outerHTML">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email Address</label>
+				<input
+					name="email"
+					type="email"
+					id="email"
+					class="border rounded-lg py-2 px-3 w-full focus:outliine-none focus:border-blue-500"
+					value="${submittedEmail}"
+					required
+				/>
+			</div>
+			<div class="${isInvalid.class}">${isInvalid.message}</div>
+			`
+		)
+	} else {
+		return res.send(
+			`
+			<div class="mb-4" hx-target="this" hx-swap="outerHTML">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email Address</label>
+				<input
+					name="email"
+					type="email"
+					id="email"
+					hx-post="/contact/email"
+					value="${submittedEmail}"
+					required
+				/>
+				<div class="${isValid.class}">${isValid.message}</div>
+			</div>
+			`
+		)
+	}
+})
+
 // Start the server
 app.listen(3000, () => {
 	console.log(`Server listening on port: http://localhost:3000`)
